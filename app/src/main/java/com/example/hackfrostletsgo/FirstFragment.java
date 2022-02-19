37e1791ddb331673d8a286a6c1b84d1ed7e68ce1
DanK-R <danon.keel-reid06@ed.cna.nl.ca>
2022-02-19 19:24:42 -0330

    hoo boy like for real

--- app/src/main/java/com/example/hackfrostletsgo/FirstFragment.java
+++ app/src/main/java/com/example/hackfrostletsgo/FirstFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -26,12 +27,24 @@ public class FirstFragment extends Fragment {
 
     }
 
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.uploadlesson.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_uploader);
+            }
+        });
+    }
+
+
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
-
 
 
 }
