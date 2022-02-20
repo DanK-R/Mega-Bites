cfab6297e08832a8dc08e106eae4d51abac36c8b
DanK-R <danon.keel-reid06@ed.cna.nl.ca>
2022-02-19 23:55:03 -0330

    Storage implemented

---
+++ app/src/main/java/com/example/hackfrostletsgo/putFile.java
@@ -0,0 +1,33 @@
+package com.example.hackfrostletsgo;
+
+public class putFile {
+
+    private String name;
+    private String url;
+
+    public putFile()
+    {
+    }
+
+    public putFile(String name, String url)
+    {
+        this.name = name;
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
